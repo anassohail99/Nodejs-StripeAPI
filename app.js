5e19a9c17adc536000a6e23b34e245d102bdf63f
anassohail99 <anas.sohailabs1@gmail.com>
2020-07-11 12:25:03 +0500

    Keys updated

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(express.static(`${__dirname}/public`));
 // index route
 app.get("/", (req, res) => {
   res.render("index", {
-    stripePstripePublishableKeys: stripePstripePublish,
+    stripePublishableKeys: stripePublishableKeys,
   });
 });
 
